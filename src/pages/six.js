b743089888121d57270f2b2428dd541925356da7
shiftsayan <sayan.chaudhry@gmail.com>
2020-12-02 22:15:08 -0500

    Hide right arrow on level 6

--- src/pages/six.js
+++ src/pages/six.js
@@ -2,8 +2,10 @@ import styles from "./index.module.scss";
 import Link from "next/link";
 import RightArrow from "../components/RightArrow";
 import LeftArrow from "../components/LeftArrow";
+import { useState } from "react";
 
 export default function Six() {
+  const [clicked, setClicked] = useState(false);
   return (
     <div className={styles.centerAlign}>
       <div className={styles.textInputContainer}>
@@ -14,14 +16,14 @@ export default function Six() {
           Click the image to view your Google Ad Settings, and take a few
           minutes to explore the “How your ads are personalized” section.
         </p>
-        <a href="https://adssettings.google.com" target="_blank">
+        <a href="https://adssettings.google.com" target="_blank" onClick={() => setClicked(true)}>
           <img src="/ad-settings.png"></img>
         </a>
-        <Link href="seven">
+        {clicked && <Link href="seven">
           <a>
-            <RightArrow></RightArrow>
+            <RightArrow />
           </a>
-        </Link>
+        </Link>}
         <Link href="five">
           <a>
             <LeftArrow></LeftArrow>
